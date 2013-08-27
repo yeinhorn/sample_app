f20a4b6b82aa873101b8780d12a30957ea12e887
yeinhorn <yaron83@gmail.com>
2013-08-27 22:06:31 +0300

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end  
   
   def new
@@ -48,6 +49,10 @@ class UsersController < ApplicationController
   end
 
 
+  
+
+
+
   private
 
   def user_params
@@ -55,13 +60,6 @@ class UsersController < ApplicationController
                                    :password_confirmation)
   end
     # Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
     
     def correct_user
       @user = User.find(params[:id])
--- spec/factories.rb
+++ spec/factories.rb
@@ -4,8 +4,16 @@ FactoryGirl.define do
     sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+
     factory :admin do
       admin true
     end
   end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
 end
+
+
